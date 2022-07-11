f1c9661b0fafae13624d74c24b633661e87c0c92
Attiq <0703thuy@gmail.com>
2022-07-11 09:12:04 +0700

    Added foo to the bar

--- music-player/src/spotify.js
+++ music-player/src/spotify.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const authEndpoint = "https://accounts.spotify.com/authorize?";
-const clientId = "ced9a75c43114a14b906dfda8c3b19a2";
+const clientId = "ced9a75c43114a14b906dfda8c3b19a2"; // //
 const redirectUri = "http://localhost:3000";
-const scopes = ["user-follow-read", "playlist-read-private"];
+const scopes = ["user-library-read", "playlist-read-private"];
 
-export const loginEndpoint = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+export const loginEndpoint = `${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
   "%20"
 )}&response_type=token&show_dialog=true`;
 
@@ -14,8 +14,8 @@ const apiClient = axios.create({
 });
 
 export const setClientToken = (token) => {
-  apiClient.interceptors.request.use(async function(config) {
-    config.headers.Authorization = "Bearer" + token;
+  apiClient.interceptors.request.use(async function (config) {
+    config.headers.Authorization = "Bearer " + token;
     return config;
   });
 };
