20a773d8b5840cd9e7c643434a8f91ce96a3ffef
Attiq <0703thuy@gmail.com>
2022-07-11 09:39:57 +0700

    Update index.js

--- music-player/src/screens/library/index.js
+++ music-player/src/screens/library/index.js
@@ -3,17 +3,22 @@ import APIKit from "../../spotify";
 
 export default function Library() {
   const [playlists, setPlaylists] = useState(null);
-useEffect (()=>{
-  APIKit.get("me/playlists").then(function (response) {
-    setPlaylists(response.data.items);
-  });
-}, []);
+  useEffect(() => {
+    APIKit.get("me/playlists").then(function (response) {
+      setPlaylists(response.data.items);
+    });
+  }, []);
   return (
     <div className="screen-container">
-      {playlists?.map((playlists) => {
-
-        <div>{playlists.name}</div>;
-      })}
+      <div className="library-body">
+        {playlists?.map((playlist) => (
+          <div className="playlist-card">
+            <img src={playlist.images[0].url} className="playlist-image" alt="Playlist-Art"/>
+            <p>{playlist.name}</p>
+            <p>{playlist.tracks.total} Songs</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
